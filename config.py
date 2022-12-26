e43bd561df03c6051ccda1b35d3dda0c755adf32
harshit_kalucha_ngi <harshit.kalucha@nextgeninvent.com>
2022-12-26 13:02:41 +0530

    changed db

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 HOST = 'https://balanceapp.pythonanywhere.com/'
-# DATABASE = dict(HOST="balanceapp.mysql.pythonanywhere-services.com", PASSWORD="Harshit@22", DATABASE="balanceapp$BalanceApp", USERNAME="balanceapp")
-DATABASE = dict(HOST="localhost", PASSWORD="Harshit@22", DATABASE="BalanceApp", USERNAME="root")
+DATABASE = dict(HOST="balanceapp.mysql.pythonanywhere-services.com", PASSWORD="Harshit@22", DATABASE="balanceapp$BalanceApp", USERNAME="balanceapp")
+#DATABASE = dict(HOST="localhost", PASSWORD="Harshit@22", DATABASE="BalanceApp", USERNAME="root")
 PASSED_LAST_DATE="include"
 PASSED_FIRST_DATE="include"
